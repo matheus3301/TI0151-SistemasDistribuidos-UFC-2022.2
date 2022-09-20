764d987b4b9afca07b7f45ad4e01d3d98d1bef2c
Vitor Rosa <vitorosa306@gmail.com>
2022-09-20 11:01:35 -0300

    add: conversionUtil

--- calculator/protocol/src/main/java/util/ConversionUtil.java
+++ calculator/protocol/src/main/java/util/ConversionUtil.java
@@ -5,7 +5,7 @@ import exceptions.InvalidRequestException;
 import messages.CalculusRequest;
 
 public class ConversionUtil {
-    public static CalculusRequest StringToObject(String s) throws InvalidRequestException {
+    public static CalculusRequest StringToObject(String s) throws InvalidRequestException{
         CalculusRequest request = new CalculusRequest();
 
         s = s.trim();
@@ -19,6 +19,37 @@ public class ConversionUtil {
 
         if(firstLine[0].toUpperCase().equals("SUM")){
             request.setOperation(Operations.SUM);
+        } else if (firstLine[0].toUpperCase().equals("SUB")){
+            request.setOperation(Operations.SUB);
+        } else if (firstLine[0].toUpperCase().equals("DIV")){
+            request.setOperation(Operations.DIV);
+        } else if (firstLine[0].toUpperCase().equals("MUL")){
+            request.setOperation(Operations.MUL);
+        } else {
+            throw new InvalidRequestException();
+        }
+
+        if(!firstLine[1].toUpperCase().equals("CP")){
+            throw new InvalidRequestException();
+        }
+
+        if(!firstLine[2].equals("1.0")){
+            throw new InvalidRequestException();
+        }
+
+        String[] headerLine;
+
+        for(int i = 1; i < lines.length; i++){
+            String blankLine = lines[i];
+            if (blankLine.equals("")){
+                if(i != 1){
+                    headerLine = lines[i-1].split(" ");
+                    request.setPrecision(Integer.parseInt(headerLine[1]));
+                }
+                request.setA(Double.parseDouble(lines[i+1]));
+                request.setB(Double.parseDouble(lines[i+2]));
+                break;
+            }
         }
 
         return request;
