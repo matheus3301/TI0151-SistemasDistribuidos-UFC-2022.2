302a0dd93ba95fd181a9848b2666bd2683129f88
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2022-11-05 16:02:15 -0300

    add: adding new fake devices

--- trabalho-02/fake-device/bedroom.py
+++ trabalho-02/fake-device/bedroom.py
@@ -1,15 +1,15 @@
 from src.device import Device
 
 device = Device(
-    "Sala de Estar",
+    "Quarto",
     [
         {'name': 'Sensor de Luminosidade', 'mean': 50, 'sd': 4},
         {'name': 'Sensor de Temperatura', 'mean': 10, 'sd': 2},
     ],
     [
-        {'name': 'Ar Condicionado'},
-        {'name': 'Televisão'}
+        {'name': 'Luz'},
+        {'name': 'Cortina'}
     ],
     3,
-    7771
+    7772
     )
